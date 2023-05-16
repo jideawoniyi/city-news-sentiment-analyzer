a8681ab86ae50185ec99a682d54190dc67b54f9c
devicesandtokens <78351990+devicesandtokens@users.noreply.github.com>
2023-05-15 20:10:35 -0600

    style and animate the stories returned- modify it2

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,12 +2,18 @@ const path = require('path');
 const webpack = require('webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     output: {
         libraryTarget: 'var',
         library: 'Client'
     },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+        },
     mode: "production",
     entry: './src/client/index.js',
     devtool: 'source-map',
@@ -20,7 +26,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
         }
         ]
     },
@@ -29,6 +35,7 @@ module.exports = {
             template: "./src/client/views/index.html",
             filename: "./index.html",
         }),
-        new CleanWebpackPlugin()
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({ filename: "[name].css" })
     ]
 }
